eb59a0de9ad783dcbfb26d9975967b009dae3706
SalmaAtefSaeid <salma.atef.ebrahim@gmail.com>
2025-04-17 18:08:13 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jasmine on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
